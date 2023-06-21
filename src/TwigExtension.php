b50c492bf96c4731cbb97d8cbb4e03362d170b98
Flavien Bucheton <f.bucheton@youlead.fr>
2023-06-21 17:26:33 +0200

    First commit

--- src/TwigExtension.php
+++ src/TwigExtension.php
@@ -10,13 +10,15 @@ declare(strict_types=1);
 namespace youleadbow\twig\Extension;
 
 use Twig\Extension\AbstractExtension;
+use youleadbow\twig\Parser\IncludeDirTokenParser;
 use youleadbow\twig\Parser\SwitchTokenParser;
 
-class SwitchTwigExtension extends AbstractExtension
+class TwigExtension extends AbstractExtension
 {
     public function getTokenParsers(): array
     {
         return [
+            new IncludeDirTokenParser(),
             new SwitchTokenParser()
         ];
     }
